c6f62b6d48dbe5a2040c0755040d06af8ff50cb4
Michael-M007 <remeta16bk@gmail.com>
2025-03-02 16:59:49 +0200

    Update train_classifier.py

--- Animal_Classification/train_classifier.py
+++ Animal_Classification/train_classifier.py
@@ -1,1 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets, transforms
+from torch import nn
+from tqdm import tqdm
+class CustomCNN(nn.Module):
+    def __init__(self):
+        super(CustomCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(128 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, 10) 
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.pool(self.conv1(x)))
+        x = self.relu(self.pool(self.conv2(x)))
+        x = self.relu(self.pool(self.conv3(x)))
+        x = x.view(-1, 128 * 8 * 8)  
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs=10):
+    best_accuracy = 0.0
+    for epoch in range(num_epochs):
+        model.train()
+        correct_preds, total_preds = 0, 0
+        for inputs, labels in tqdm(train_loader, desc=f'Epoch {epoch+1}/{num_epochs}', leave=False):
+            inputs, labels = inputs.cuda(), labels.cuda()
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            _, preds = torch.max(outputs, 1)
+            correct_preds += torch.sum(preds == labels).item()
+            total_preds += labels.size(0)
+        accuracy = correct_preds / total_preds
+        print(f'Epoch {epoch+1}/{num_epochs}, Accuracy: {accuracy:.4f}')
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            torch.save(model.state_dict(), 'best_animal_classifier.pth')
+            print(f'Saved best model with accuracy {best_accuracy:.4f}')
+
+def main():
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    dataset = datasets.ImageFolder('path_to_animals_10_dataset', transform=transform)
+
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+    model = CustomCNN().cuda()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    train_model(model, train_loader, val_loader, criterion, optimizer)
+
+if __name__ == '__main__':
+    main()
 
